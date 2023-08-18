b00dcc103337ab07dd8522fc2a6ac9babe5bfa07
SubhaKrishnamurthy <subha.sk9194@gmail.com>
2023-08-18 16:02:12 +0530

    commiting testClass

---
+++ src/test/java/TestPeckage/TestClass.java
@@ -0,0 +1,21 @@
+package TestPeckage;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass {
+    @Test
+public void JenkinsTest(){
+
+        System.out.println("Testing the Jenkins program execution.");
+}
+//public static void main(String[] args)
+
+
+
+
+
+
+}
